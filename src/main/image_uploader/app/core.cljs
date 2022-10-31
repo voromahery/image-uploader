b15ab9767ebd362db0b3d769005e5983cd6d8dc5
Developer <daniel.her@onja.org>
2022-10-31 16:16:44 +0300

    Show image url

--- src/main/image_uploader/app/core.cljs
+++ src/main/image_uploader/app/core.cljs
@@ -1,11 +1,10 @@
 (ns image-uploader.app.core
   (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [cljs-http.client :as http]
-            [cljs.core.async :refer [<!]])
   (:require [reagent.core :as r]
             [reagent.dom :as rdom]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]
             [goog.string :as str]
-            [promesa.core :as p]
             [komponentit.clipboard :as clipboard]
             [image-uploader.app.views.card :refer [card]]
             [image-uploader.app.views.loader :refer [loader]]))
@@ -13,7 +12,7 @@
 ;; --- App State ---
 (def loading (r/atom false))
 (def image-preview (r/atom nil))
-(def url (r/atom ""))
+(def url (r/atom nil))
 (def is-copied (r/atom false))
 (def selected-file (r/atom ""))
 (def image-name (r/atom ""))
@@ -22,29 +21,29 @@
 (defn generate-form-data [params]
   (let [form-data (js/FormData.)]
     (doseq [[k v] params]
-      (js/console.log k v "KV")
       (.append form-data (name k) v))
     form-data))
 
 (defn uploat-to-imgbb [file]
+  (reset! loading true)
   (go (let [response (<! (http/post "https://api.imgbb.com/1/upload?key=296e8ac65bb5984470ac071ce3415da6"
                                     {:body (generate-form-data {:image file})
                                      :with-credentials? false}))]
-        (js/console.log file)
-        (js/console.log (generate-form-data {:file file}) "BODY"))))
+
+        (reset! url (get (get (get (:body response) :data) :thumb) :url))
+        (reset! loading false))))
+
 
 (defn upload-image [event]
   (let [file (-> event .-target .-files (aget 0))
         file-reader (js/FileReader.)]
-
     (uploat-to-imgbb (-> event .-target .-files (aget 0)))
 
     (reset! image-name (-> event .-target .-files (aget 0) .-name))
 
     (set! (.-onload file-reader)
           (fn [event]
-            (reset! image-preview (-> event .-target .-result))
-            (reset! url (str (-> js/window .-location .-href) (-> event .-target .-result)))))
+            (reset! image-preview (-> event .-target .-result))))
     (.readAsDataURL file-reader file)))
 
 (defn copy-url []
@@ -55,7 +54,7 @@
 (defn app []
   [:div.main
    [:div.wrapper
-    (if (= @loading true)
+    (if @loading
       [loader]
       [card upload-image image-preview copy-url url is-copied])]
    [:footer
